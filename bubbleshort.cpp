62d310319527ac2b56354d4ae2f29ae87b3f7c6e
aliyanadaa <aliyanadaq@gmail.com>
2026-03-06 15:53:26 +0700

    menambahkan prosedur input

--- bubbleshort.cpp
+++ bubbleshort.cpp
@@ -3,3 +3,26 @@ using namespace std;
 
 int a[20];
 int n;
+
+void input()
+{
+    cout << "Masukkan panjang array : ";
+    cin >> n;
+
+    if (n > 20)
+    {
+        cout << "Error : Maksimal input array adalah 20\n";
+        return;
+    }
+
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen Array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Masukkan nilai ke - " << i + 1 << " : ";
+        cin >> a[i];
+    }
+}
